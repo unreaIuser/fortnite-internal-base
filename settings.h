b58dc4c176acca979be5ff027652efcf9abf3034
unreaIuser <93293947+unreaIuser@users.noreply.github.com>
2022-12-31 12:32:35 +0100

    Add files via upload

---
+++ settings.h
@@ -0,0 +1,17 @@
+#pragma once
+
+namespace Settings
+{
+	bool Crosshair = false;
+	bool FovChanger = false;
+	bool FovCircle = false;
+	bool ActorLoop = false;
+	bool snaplines = false;
+
+	static float FOV = 125.0f;
+	
+
+	int fov_value = 40.f;
+	int smooth = 3;
+	int aimbone = 66;
+}
---
+++ spoofcall.h
@@ -0,0 +1,208 @@
+#pragma once
+
+#pragma once
+#define RVA(addr, size) ((uintptr_t)((UINT_PTR)(addr) + *(PINT)((UINT_PTR)(addr) + ((size) - sizeof(INT))) + (size)))
+#define M_PI	3.14159265358979323846264338327950288419716939937510
+#define _CRT_SECURE_NO_WARNINGS
+#define COBJMACROS
+#define WIN32_LEAN_AND_MEAN
+#define MAX_VERTEX_BUFFER 512 * 1024
+#define MAX_INDEX_BUFFER 128 * 1024
+#define NK_INCLUDE_STANDARD_IO
+#define NK_INCLUDE_STANDARD_VARARGS
+#define NK_INCLUDE_DEFAULT_ALLOCATOR
+#define NK_INCLUDE_FIXED_TYPES
+#define NK_INCLUDE_VERTEX_BUFFER_OUTPUT
+#define NK_INCLUDE_FONT_BAKING
+#define NK_INCLUDE_DEFAULT_FONT
+
+#include <misc/lazy.h>
+#include <windows.h>
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+#include <time.h>
+#include <math.h>
+#include <D3D11.h>
+#include <codecvt>
+#include <Psapi.h>
+#include <list>
+
+#include <wchar.h>
+#include <tchar.h>
+#include <conio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <ostream>
+#include <regex>
+#include <winuser.h>
+#include <WinReg.h>
+#include <winternl.h>
+
+#include <TlHelp32.h>
+#include <random>
+#include <ctime>
+#include <urlmon.h>
+
+namespace detail
+{
+	extern "C" void* _spoofer_stub();
+
+	template <typename Ret, typename... Args>
+	static inline auto shellcode_stub_helper(
+		const void* shell,
+		Args... args
+	) -> Ret
+	{
+		auto fn = (Ret(*)(Args...))(shell);
+		return fn(args...);
+	}
+
+	template <std::size_t Argc, typename>
+	struct argument_remapper
+	{
+		template<
+			typename Ret,
+			typename First,
+			typename Second,
+			typename Third,
+			typename Fourth,
+			typename... Pack
+		>
+		static auto do_call(const void* shell, void* shell_param, First first, Second second,
+			Third third, Fourth fourth, Pack... pack) -> Ret
+		{
+			return shellcode_stub_helper< Ret, First, Second, Third, Fourth, void*, void*, Pack... >(shell, first, second, third, fourth, shell_param, nullptr, pack...);
+		}
+	};
+
+	template <std::size_t Argc>
+	struct argument_remapper<Argc, std::enable_if_t<Argc <= 4>>
+	{
+		template<
+			typename Ret,
+			typename First = void*,
+			typename Second = void*,
+			typename Third = void*,
+			typename Fourth = void*
+		>
+		static auto do_call(
+			const void* shell,
+			void* shell_param,
+			First first = First{},
+			Second second = Second{},
+			Third third = Third{},
+			Fourth fourth = Fourth{}
+		) -> Ret
+		{
+			return shellcode_stub_helper<
+				Ret,
+				First,
+				Second,
+				Third,
+				Fourth,
+				void*,
+				void*
+			>(
+				shell,
+				first,
+				second,
+				third,
+				fourth,
+				shell_param,
+				nullptr
+				);
+		}
+	};
+}
+uintptr_t Iamgayaddr = (uintptr_t)SAFE_CALL(GetModuleHandleA)(("FortniteClient-Win64-Shipping.exe"));
+
+template <typename Ret, typename... Args>
+static inline auto SpoofCall(Ret(*fn)(Args...), Args... args) -> Ret
+{
+	static const void* jmprbx = nullptr;
+	if (!jmprbx) {
+		const auto ntdll = reinterpret_cast<const unsigned char*>(Iamgayaddr);
+		const auto dos = reinterpret_cast<const IMAGE_DOS_HEADER*>(ntdll);
+		const auto nt = reinterpret_cast<const IMAGE_NT_HEADERS*>(ntdll + dos->e_lfanew);
+		const auto sections = IMAGE_FIRST_SECTION(nt);
+		const auto num_sections = nt->FileHeader.NumberOfSections;
+
+		constexpr char section_name[5]{ '.', 't', 'e', 'x', 't' };
+		const auto     section = std::find_if(sections, sections + num_sections, [&](const auto& s) {
+			return std::equal(s.Name, s.Name + 5, section_name);
+			});
+
+		constexpr unsigned char instr_bytes[2]{ 0xFF, 0x26 };
+		const auto              va = ntdll + section->VirtualAddress;
+		jmprbx = std::search(va, va + section->Misc.VirtualSize, instr_bytes, instr_bytes + 2);
+	}
+
+	struct shell_params
+	{
+		const void* trampoline;
+		void* function;
+		void* rdx;
+	};
+
+	shell_params p
+	{
+		jmprbx,
+		reinterpret_cast<void*>(fn)
+	};
+
+	using mapper = detail::argument_remapper<sizeof...(Args), void>;
+	return mapper::template do_call<Ret, Args...>((const void*)&detail::_spoofer_stub, &p, args...);
+}
+
+namespace SpoofRuntime {
+	inline float acosf_(float x)
+	{
+		return SpoofCall(acosf, x);
+	}
+
+
+	inline float atan2f_(float x, float y)
+	{
+		return SpoofCall(atan2f, x, y);
+	}
+
+	inline float sqrtf_(float x)
+	{
+		union { float f; uint32_t i; } z = { x };
+		z.i = 0x5f3759df - (z.i >> 1);
+		z.f *= (1.5f - (x * 0.5f * z.f * z.f));
+		z.i = 0x7EEEEEEE - z.i;
+		return z.f;
+	}
+
+
+	double powf_(double x, int y)
+	{
+		double temp;
+		if (y == 0)
+			return 1;
+		temp = powf_(x, y / 2);
+		if ((y % 2) == 0) {
+			return temp * temp;
+		}
+		else {
+			if (y > 0)
+				return x * temp * temp;
+			else
+				return (temp * temp) / x;
+		}
+	}
+
+	inline float cosf_(float x)
+	{
+		return SpoofCall(cosf, x);
+	}
+
+	inline float sinf_(float x)
+	{
+		return SpoofCall(sinf, x);
+	}
+
+}
